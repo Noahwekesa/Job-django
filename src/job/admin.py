ae31740c5daf9a30a7f63fc3d61802228b231b6c
Noah Wekesa <57433233+Noahwekesa@users.noreply.github.com>
2023-09-23 10:22:10 +0300

    Delete src directory

--- src/job/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import job
-from users.models import Jobseekerjobmap
-# Register your models here.
-
-admin.site.register(job)
-
-class JobseekerjobmapAdmin(admin.ModelAdmin): 
-    model = Jobseekerjobmap
