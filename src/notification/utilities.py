9c8741c45903ec91647d574386a74822ecc43708
noahwksa <noahwekesa0gmail.com>
2023-10-30 21:07:08 +0300

    done

--- src/notification/utilities.py
+++ src/notification/utilities.py
@@ -1,4 +1,16 @@
 from .models import Notification
 
 def create_notification(request, to_user,  notification_type, extra_id=0):
-    notificaiton = Notification.objects.create(to_user=to_user, notification_type=notification_type,  user=request.user, extra_id=extra_id)
+     notificaiton = Notification.objects.create(to_user=to_user, notification_type=notification_type,  user=request.user, extra_id=extra_id)
+
+# def create_message_notification(sender, receiver):
+#     Notification.objects.create(to_user=receiver, notification_type=Notification.MESSAGE, user=sender)
+
+# def create_notification(sender, receiver, notification_type, extra_id=0):
+#     Notification.objects.create(to_user=receiver, notification_type=notification_type, user=sender, extra_id=extra_id)
+
+# def create_message_notification(sender, receiver):
+#     Notification.objects.create(to_user=receiver, notification_type=Notification.MESSAGE, user=sender)
+
+# def create_notification(sender, receiver, notification_type, extra_id=0):
+#     Notification.objects.create(to_user=receiver, notification_type=notification_type, user=sender, extra_id=extra_id)
--- src/notification/views.py
+++ src/notification/views.py
@@ -3,6 +3,25 @@ from .models import Notification
 from django.contrib.auth.decorators import login_required
 
 @login_required
+# def notifications(request):
+#     goto = request.GET.get('goto', '' )
+#     notification_id = request.GET.get('notification', 0)
+#     extra_id = request.GET.get('extra_id', 0)
+
+#     if goto != '':
+#         notification = Notification.objects.get(pk=notification_id)
+#         notification.is_read = True
+#         notification.save()
+
+#         if notification.notification_type == Notification.MESSAGE:
+#             if request.user.is_employer:
+#                 return redirect('view_application', applyjob_id=notification.extra_id)
+#             if request.user.is_employer:
+#                 return redirect('view_application', applyjob_id=notification.extra_id)
+#         elif notification.notification_type == Notification.APPLICATION:
+#             if request.user.is_employer:
+#                 return redirect('view_application', applyjob_id=notification.extra_id)
+#     return render(request, 'notification/notifications.html')
 def notifications(request):
     goto = request.GET.get('goto', '' )
     notification_id = request.GET.get('notification', 0)
@@ -11,11 +30,22 @@ def notifications(request):
     if goto != '':
         notification = Notification.objects.get(pk=notification_id)
         notification.is_read = True
+        # user=request.user,
         notification.save()
 
         if notification.notification_type == Notification.MESSAGE:
-            return redirect('view_application', application_id=notification.extra_id)
+            if request.user.is_employer:
+                # Redirect employers to a specific view for messages
+                return redirect('view_application', applyjob_id=notification.extra_id)
+            elif request.user.is_jobseeker:
+                # Redirect job seekers to a different view for messages
+                return redirect('view_application', applyjob_id=notification.extra_id)
         elif notification.notification_type == Notification.APPLICATION:
-            return redirect('view_application', application_id=notification.extra_id)
-    return render(request, 'notification/notifications.html')
+            if request.user.is_employer:
+                # Redirect employers to a specific view for applications
+                return redirect('view_application', applyjob_id=notification.extra_id)
+            elif request.user.is_jobseeker:
+                # Redirect job seekers to a different view for applications
+                return redirect('view_application', applyjob_id=notification.extra_id)
 
+    return render(request, 'notification/notifications.html')
